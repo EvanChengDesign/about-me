0ed63770795e54feb7aeebc761c1dd54a764250f
Evan Cheng <evanchengdesign@gmail.com>
2024-02-07 17:23:28 -0500

    Add js

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// What is code all about?
+//INPUT then PROCESS thn OUTPUT
+
+
+// Variables
+
+// Declare a variable called "father" and assign it the value of "Hugo"
+// var, let, const
+// constant - never changes
+
+let dad = "Hugo";
+
+// const dad ;
+
+//print whatever is inside () to the console
+console.log(dad);
+
+// Variables can be changed (replaced)
+// father = "Hugo";
+
+console.log(dad);
+
+// Conditional logic -- IFTTT "If this then that"
+// Checks the status of whatever is in the () and if its true, runs the {}
+// If it is fales, runs whatever is in the else {}
+//if the age is > 50, make fun of the person, otherwise, tease them
+let age = 49;
+
+if (age > 50) {
+    console.log("LOL, you're on the downslope!")
+}
+    else {
+        console.log("enjoy your youth while it lasts!")
+    }
