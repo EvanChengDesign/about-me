ca59ef07e6b335293025939c517da82d25775af8
Evan Cheng <evanchengdesign@gmail.com>
2024-02-07 19:49:16 -0500

    add style and class for popup.js

--- js/PopUp.js
+++ js/PopUp.js
@@ -3,5 +3,8 @@
 //Prompt will pop up a window and ask a question
 
     let hello = prompt("Hello, what is your name?");
-    let sentence = "Welcome to my page! Have a great day!";
+    let uppercaseName = hello.toUpperCase(); // Convert the name to uppercase
+    let sentence = "<p class='welcome-message'>WELCOME TO MY PAGE " + uppercaseName + "! HAVE A GREAT DAY!</p>";
     document.write(sentence);
+
+   
